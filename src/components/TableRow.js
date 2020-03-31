9e8dde79e3463bfebf6ff707f38f382720c39afc
Monica <monica.dixon8@gmail.com>
2020-03-31 11:00:06 -0500

    all API info is going into correct location

--- src/components/TableRow.js
+++ src/components/TableRow.js
@@ -3,14 +3,21 @@ import React from "react";
 function TableRow({ usersToDisplay }) {
   console.log(usersToDisplay[0]);
   //In the table I want to create a row for each employee that needs to be displayed.
+
   return (
     <tbody>
       {usersToDisplay.map(emp => (
         <tr>
-          <td>{emp.thumbnail}</td>
-          <td>{emp.first}</td>
-          <td>{emp.last}</td>
+          <td>
+            <img
+              src={emp.picture.thumbnail}
+              alt={`${emp.name.first} ${emp.name.last}`}
+            />
+          </td>
+          <td>{emp.name.first}</td>
+          <td>{emp.name.last}</td>
           <td>{emp.phone}</td>
+          <td>{emp.email}</td>
         </tr>
       ))}
     </tbody>
